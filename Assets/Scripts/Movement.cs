98437ec72f8307e592d8b2508d3c60b26f4dfefb
Kozun <koozunek@gmail.com>
2016-12-10 23:15:00 +0100

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour {
     public float Speed;
 
     public float RunSpeed;
+    [SerializeField]
+    private float maxVelocityChange = 10.0f;
 
     private Vector3 direction;
     public Vector3 Direction
@@ -39,18 +41,20 @@ public class Movement : MonoBehaviour {
         if(direction.magnitude == 0)
         {
             animator.SetInteger("State", 0);
-            //dont ask
-            body.velocity = transform.TransformVector(direction) * Speed;
+
+            Move(0);
         }
         else if(!Input.GetKey(KeyCode.LeftShift))
         {
             animator.SetInteger("State", 1);
-            body.velocity = transform.TransformVector(direction) * Speed;
+
+            Move(Speed);
         }
         else
         {
             animator.SetInteger("State", 2);
-            body.velocity = transform.TransformVector(direction) * RunSpeed;
+
+            Move(RunSpeed);
         }
         transform.Rotate(transform.up, Rotation.x);
         float angle = Head.localRotation.eulerAngles.x + Rotation.y;
@@ -59,6 +63,22 @@ public class Movement : MonoBehaviour {
         if (angle < -180)
             angle += 360;
         Head.localEulerAngles = new Vector3(Mathf.Clamp(angle, -90, 90), 0, 0);
+    }
+
+    void Move(float speed)
+    {
+
+            Vector3 targetVelocity = new Vector3(direction.x, 0, direction.z);
+	        targetVelocity = transform.TransformDirection(targetVelocity);
+	        targetVelocity *= speed;
+ 
+	        // Apply a force that attempts to reach our target velocity
+	        Vector3 velocity = body.velocity;
+	        Vector3 velocityChange = (targetVelocity - velocity);
+	        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+	        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+	        velocityChange.y = 0;
+	        body.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 
 }
