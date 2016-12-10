8d6e74b02237b4d60cf1a5f59d5976b9e7eb6f5c
juna8001 <juna8001@gmail.com>
2016-12-10 18:21:48 +0100

    ŚWIECĄCE POCISKI

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour {
+
+    public float ExistTime;
+
+    void Start()
+    {
+        StartCoroutine(destroyer());
+    }
+
+    IEnumerator destroyer()
+    {
+        yield return new WaitForSeconds(ExistTime);
+        
+        Destroy(gameObject);
+    }
+}
