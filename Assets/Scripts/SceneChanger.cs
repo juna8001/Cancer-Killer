2aaac9b1b86d24de495f7624ae0cecfcbd0a6e2c
Kozun <koozunek@gmail.com>
2016-12-11 04:17:33 +0100

    Scene Changer

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	[SerializeField]
+    private String sceneName;
+    [SerializeField]
+    private GameObject faderCanvas;
+    [SerializeField]
+    private float delay = 1f;
+    
+    public void ChangeScene()
+    {
+        DontDestroyOnLoad(faderCanvas);
+        DontDestroyOnLoad(gameObject);
+        faderCanvas.GetComponentInChildren<Animator>().SetTrigger("FadeIn");
+        Invoke("ChangeSceneBro", delay);
+    }
+
+    void ChangeSceneBro()
+    {
+        SceneManager.LoadScene(sceneName);
+        faderCanvas.GetComponentInChildren<Animator>().SetTrigger("FadeOut");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeScene();
+        }
+    }
+}
